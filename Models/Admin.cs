8fe8bf39988231c8df8a1107dcad4a26f50f4aae
Matin Amin Sabouti <matinaminsabouri70@gmail.com>
2023-09-02 10:56:20 +0330

    Design Admin model

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -1,7 +1,18 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace SibSalamat.Models;
 
 public class Admin
 {
-    
+    [Required]
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string AdminID { get; set; }
+
+    [Required] [MinLength(3)] public string Name { get; set; }
+    [Required] [MinLength(5)] public string Password { get; set; }
+    [Required] [EmailAddress] public string Email { get; set; }
 }
