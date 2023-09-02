98e5aa7306971d26863374a073823d97a2659c97
Matin Amin Sabouti <matinaminsabouri70@gmail.com>
2023-09-02 19:48:28 +0330

    Creat doctor's receptionist dropdown section

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -15,4 +15,5 @@ public class Admin
     [Required] [MinLength(3)] public string Name { get; set; }
     [Required] [MinLength(5)] public string Password { get; set; }
     [Required] [EmailAddress] public string Email { get; set; }
+    [Required] public string NezamPezeshki { get; set; }
 }
