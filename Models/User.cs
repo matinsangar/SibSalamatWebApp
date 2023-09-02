afafb8873a154b4ab82aef57597325272e3276b0
Matin Amin Sabouti <matinaminsabouri70@gmail.com>
2023-09-02 10:58:08 +0330

    Design User model

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace SibSalamat.Models;
+
+public class User
+{
+    [Required]
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string UserID { get; set; }
+
+    [Required] [MinLength(3)] public string Name { get; set; }
+    [Required] [MinLength(5)] public string Password { get; set; }
+    [Required] [EmailAddress] public string Email { get; set; }
+}
