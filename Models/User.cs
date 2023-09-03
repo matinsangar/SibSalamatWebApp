ecd79c5b36c2903d1eb315ec58f8292597ecc236
Matin Amin Sabouti <matinaminsabouri70@gmail.com>
2023-09-03 10:49:12 +0330

    Design user signup and login section

--- Models/User.cs
+++ Models/User.cs
@@ -16,4 +16,5 @@ public class User
     [Required] [MinLength(3)] public string Name { get; set; }
     [Required] [MinLength(5)] public string Password { get; set; }
     [Required] [EmailAddress] public string Email { get; set; }
+    [Required] [StringLength(4)] public string NationalCode { get; set; } //کدملی
 }
