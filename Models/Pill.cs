36b2f9accd5869040053508d69d2bb28d11359e5
Matin Amin Sabouti <matinaminsabouri70@gmail.com>
2023-09-09 12:22:20 +0330

    Update Pill model and Razor with some style changes

--- Models/Pill.cs
+++ Models/Pill.cs
@@ -5,9 +5,15 @@ namespace SibSalamat.Models;
 
 public class Pill
 {
-    [BsonId]
-    public ObjectId Id { get; set; }
+    [BsonId] public ObjectId Id { get; set; }
     public string Name { get; set; }
     public decimal Price { get; set; }
     public int ImageNumber { get; set; }
+    public string? Description { get; set; } = "no info provided for this pill";
+
+    public int? AvailableCount { get; set; } = 0;
+
+    //Name and NezamPezeshki   
+    public string Provider;
+    public string PharmacyNumber;
 }
